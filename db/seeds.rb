d77f03260f2995d470d7ae5cdbbc23e16c340792
root <muhammed.gamal@kickstartinteractive.net>
2020-02-02 11:21:43 +0200

    model migration  + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Eve", password: "123456")
+User.create(username: "Steve", password: "123456")
+User.create(username: "Shady", password: "123456")
+User.create(username: "Bill", password: "123456")
